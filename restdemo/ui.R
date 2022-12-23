d92ddacbd33c3ae84b24454928840d739a6268ea
lbalarin <luigi.balarinni@gmail.com>
2022-12-23 00:31:54 -0500

    Stable working on shinyapps.io

--- restdemo/ui.R
+++ restdemo/ui.R
@@ -6,44 +6,30 @@
 #
 #    http://shiny.rstudio.com/
 #
-library(shiny)
-library(shinydashboard)
 
-shinyUI(dashboardPage(
-    dashboardHeader("Test"),
-    dashboardSidebar("Side"),
-    dashboardBody("Main")
-  )
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("NYC Restaurants and Inspections"),
+  fluidRow(
+      column(3,
+             selectInput("boro", label = "Select Borough",
+                         choices = list(
+                           `All Boros` = "All",
+                           `Bronx` = "Bronx",
+                           `Brooklyn` = "Brooklyn",
+                           `Manhattan` = "Manhattan",
+                           `Queens`= "Queens",
+                           `Staten Island`= "Staten Island"
+                           ),
+                         selected = "All", selectize=FALSE)
+             ),
+      column(9, h1("Histogram of Inspection Grades from All 5 Boroughs"),
+             plotOutput("placeholder")
+             ),
+
+      ),
+  fluidRow(column(12, h1("NYC Restaurant Grade Database"),dataTableOutput("ex1"))),
+  fluidRow(column(12, h1("Inspection Report Database"),dataTableOutput("ex2")))
+    )
 )
-# shinyUI(fluidPage(
-# 
-#     # Application title
-#     titlePanel("NYC Restaurants and Inspections"),
-#   fluidRow(
-#       column(3,
-#              # "sidebar", # LEFT PANE
-#              textInput("name", label = "Restaurant Name"),
-#              selectInput("boro", label = "Select Borough", 
-#                          choices = list(
-#                            `All Boros` = "All",
-#                            `Bronx` = "Bronx",
-#                            `Brooklyn` = "Brooklyn",
-#                            `Manhattan` = "Manhattan",
-#                            `Queens`= "Queens",
-#                            `Staten Island`= "Staten Island"
-#                            ), 
-#                          selected = "Bronx", selectize=FALSE),
-#              textOutput("result")
-#              
-#              # actionButton("action", label = "Search")
-#              ),
-#       column(9, 
-#              plotOutput("placeholder")
-#              ),
-#       
-#       ),
-#   fluidRow(column(12, dataTableOutput("ex1")))
-#   
-#     )
-#     # fluidRow(column(2, verbatimTextOutput("value")))
-# )
